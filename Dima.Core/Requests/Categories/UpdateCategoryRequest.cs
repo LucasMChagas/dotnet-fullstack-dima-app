f56f032783e04d1e380c603f38c0ac9ceab8d2f4
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2025-03-02 18:24:01 -0300

    feat: category requests created

---
+++ Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Categories;
+
+public class UpdateCategoryRequest : Request
+{
+    [Required(ErrorMessage = "Título inválido")]
+    [MaxLength(80, ErrorMessage = "O título deve conter no máximo 80 caracteres")]
+    public string Title { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Descrição deve ser fornecida")]
+    public string Description { get; set; } = string.Empty;
+}
