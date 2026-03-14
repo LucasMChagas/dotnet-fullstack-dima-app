50ac7dac5e5546b7a2cb8771aceeb013c16f0578
Lucas Miranda <lumidach@gmail.com>
2026-03-13 23:37:07 -0300

    feat: added transactions endpoints

--- Dima.Api/Endpoints/Endpoint.cs
+++ Dima.Api/Endpoints/Endpoint.cs
@@ -1,5 +1,6 @@
 using Dima.Api.Common.Api;
 using Dima.Api.Endpoints.Categories;
+using Dima.Api.Endpoints.Transactions;
 
 namespace Dima.Api.Endpoints;
 
@@ -17,6 +18,14 @@ public static class Endpoint
             .MapEndpoint<DeleteCategoryEndpoint>()
             .MapEndpoint<GetCategoryByIdEndpoint>()
             .MapEndpoint<GetAllCategoriesEndpoint>();
+        
+        endpoints.MapGroup("/v1/transactions")
+            .WithTags("transactions")
+            .MapEndpoint<CreateTransactionEndpoint>()
+            .MapEndpoint<UpdateTransactionEndpoint>()
+            .MapEndpoint<DeleteTransactionEndpoint>()
+            .MapEndpoint<GetTransactionByIdEndpoint>()
+            .MapEndpoint<GetTransactionByPeriodEndpoint>();
     }
     private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) where TEndpoint : IEndpoint
     {
