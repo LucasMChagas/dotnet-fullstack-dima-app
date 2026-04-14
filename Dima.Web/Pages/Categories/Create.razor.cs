16f6adae240d06d3bfdb4ce579676c62ef77a1be
Lucas <lumidach@gmail.com>
2026-04-14 19:07:30 -0300

    feat(frontEnd): create categoy page

---
+++ Dima.Web/Pages/Categories/Create.razor.cs
@@ -0,0 +1,47 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Categories;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Categories;
+
+public partial class CreateCategoryPage : ComponentBase
+{
+    public bool IsBusy { get; set; } = false;
+    public CreateCategoryRequest? Request { get; set; } = new();
+
+    [Inject]
+    public ICategoryHandler Handler { get; set; } = null!;
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; }
+    
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var result = await Handler.CreateAsync(Request);
+            if (result.IsSuccess)
+            {
+                Snackbar.Add(result.Message, Severity.Success);
+                NavigationManager.NavigateTo("/categorias");
+            }
+            else
+                Snackbar.Add(result.Message, Severity.Error);
+        }
+        catch (Exception exception)
+        {
+            Snackbar.Add(exception.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+    
+}
