c44de2c649c4985feae4eab752bb0dccc2b2bb35
Lucas <lumidach@gmail.com>
2026-04-26 12:06:47 -0300

    feat: interface report handler created

--- Dima.Api/Data/AppDbContext.cs
+++ Dima.Api/Data/AppDbContext.cs
@@ -24,6 +24,8 @@ public class AppDbContext : IdentityDbContext<
     }
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Transaction> Transactions { get; set; } = null!;
+    
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
