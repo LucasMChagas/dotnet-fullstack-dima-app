b2646ebdf5e2e5c6fdf6786aa0938383360931af
Lucas <lumidach@gmail.com>
2026-05-06 12:22:45 -0300

    feat: added create order endpoint

---
+++ Dima.Api/Endpoints/Orders/CreateOrderEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class CreateOrderEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/", HandleAsync)
+            .WithName("Order: CreateOrder")
+            .WithDescription("Cria um novo pedido")
+            .WithSummary("Cria um novo pedido")
+            .WithOrder(2)
+            .Produces<Response<Order?>>(StatusCodes.Status201Created);
+
+    private static async Task<IResult> HandleAsync(
+        IOrderHandler handler,
+        CreateOrderRequest request,
+        ClaimsPrincipal user)
+    {
+        request.UserId = user.Identity!.Name ?? String.Empty;
+        var result = await handler.CreateOrderAsync(request);
+        return result.IsSuccess
+            ? TypedResults.Created($"v1/orders/{result.Data?.Number}", result)
+            : TypedResults.BadRequest(result);
+    }
+}
