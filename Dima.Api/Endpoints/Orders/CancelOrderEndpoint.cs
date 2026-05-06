2293fe7c6b16357694ea21b6383ba0398c5d9255
Lucas <lumidach@gmail.com>
2026-05-06 12:13:14 -0300

    feat: added order endpoint

---
+++ Dima.Api/Endpoints/Orders/CancelOrderEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class CancelOrderEndpoint :IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/{id}/cancel,", HandleAsync)
+            .WithName("Cancel: cancel order")
+            .WithSummary("Cancela um pedido")
+            .WithDescription("Cancela um pedido")
+            .WithOrder(1)
+            .Produces<Response<Order?>>(); 
+    
+    private static async Task<IResult> HandleAsync(
+        IOrderHandler handler, 
+        long id,
+        ClaimsPrincipal user)
+    {
+        var request = new CancelOrderRequest
+        {
+            Id = id,
+            UserId = user.Identity?.Name ?? string.Empty,
+        };
+
+        var result = await handler.CanceOrderlAsync(request);
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest();
+    }
+}
