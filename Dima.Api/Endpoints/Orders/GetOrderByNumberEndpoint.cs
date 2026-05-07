35faf794eb68294959015f1686a11c18e2096076
Lucas <lumidach@gmail.com>
2026-05-07 12:51:59 -0300

    feat: added get order by number endpoint

---
+++ Dima.Api/Endpoints/Orders/GetOrderByNumberEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class GetOrderByNumberEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/{number}", HandleAsync)
+            .WithName("Order: Get by number")
+            .WithSummary("Obter pedido pelo número")
+            .WithDescription("Obter pedido pelo número")
+            .WithOrder(6)
+            .Produces<Response<Order?>>();
+
+    private static async Task<IResult> HandleAsync(
+        ClaimsPrincipal user,
+        IOrderHandler handler,
+        string number)
+    {
+        var request = new GetOrderByNumberRequest
+        {
+            Number = number,
+            UserId = user.Identity!.Name ?? string.Empty
+        };
+
+        var result = await handler.GetOrderByNumberAsync(request);
+        
+        return result.IsSuccess 
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
