a2c21c25e643b4f89ef593ebb65fffcb8d5629c6
Lucas <lumidach@gmail.com>
2026-05-07 13:00:27 -0300

    feat: added create get product by slug endpoint

---
+++ Dima.Api/Endpoints/Orders/GetProductBySlugEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class GetProductBySlugEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/{slug}", HandleAsync)
+            .WithName("Product: Get by Slug")
+            .WithSummary("Obter produto pelo slug")
+            .WithDescription("Obter produto pelo slug")
+            .WithOrder(7)
+            .Produces<Response<Product?>>();
+    
+    private static async Task<IResult> HandleAsync(
+        IProductHandler handler,
+        string slug)
+    {
+        var request = new GetProductBySlugRequest
+        {
+            Slug = slug
+        };
+
+        var result = await handler.GetBySlugAsync(request);
+        
+        return result.IsSuccess 
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
